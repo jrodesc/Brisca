1bca307d4910b47e7d91457c9f40ad921b13de43
Jose Luis <jrodesc1511@gmail.com>
2025-04-15 11:20:57 +0200

    modificacion brisca

--- src/Controladores/Brisca.java
+++ src/Controladores/Brisca.java
@@ -5,6 +5,7 @@ import EstructuraDatos.Jugable;
 import EstructuraDatos.JugadorHumano;
 import EstructuraDatos.JugadorPC;
 import EstructuraDatos.Naipe;
+import Vistas.InterfazConsola;
 
 public class Brisca implements Jugable{
     private JugadorHumano jugadorA;
@@ -14,21 +15,53 @@ public class Brisca implements Jugable{
 
     private Naipe muestra;
 
+    public Brisca(String nombre, String dificultad) {
+        nombre = jugadorA.getNombre();
+        baraja = new Baraja("Española");
+    }
+
+    public void ronda(boolean quienEmpieza) {
+        //si boolean true empieza jugadorA
+        InterfazConsola.cartaJugadaOponente(jugadorB.getNombre(), muestra);
+        InterfazConsola.pedirCarta(jugadorA);
+
+    }
+
     @Override
     public void inizializar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inizializar'");
+        //se inicializa el juego reparte 3 cartas a cada jugador
+        for(int i = 0; i<3; i++) {
+            jugadorA.recibirCarta(baraja.repartirCarta());
+            jugadorB.recibirCarta(baraja.repartirCarta());
+        }
+        muestra = baraja.copiaPrimeraCarta();
+
+        jugar();
     }
 
     @Override
     public void jugar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'jugar'");
+        do {
+            jugadorA.getMano();
+            ronda(true);
+            if (baraja.cartasRestantes()>0) {
+                baraja.repartirCarta();
+            }
+            comprobarGanador();
+        } while(baraja.cartasRestantes() > 0);
     }
 
     @Override
     public void comprobarGanador() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'comprobarGanador'");
+    }
+
+    public boolean ganadorMano(Naipe naipeA, Naipe naipeB, boolean quienEmpezo) {
+        if (naipeA.getPuntuacion() > naipeB.getPuntuacion()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/Vistas/InterfazConsola.java
+++ src/Vistas/InterfazConsola.java
@@ -1,30 +1,31 @@
 package Vistas;
 
 
+import EstructuraDatos.Jugador;
 import EstructuraDatos.JugadorHumano;
 import EstructuraDatos.Naipe;
 import Utildiades.ES;
 
 public class InterfazConsola {
-    public void pedirCarta(JugadorHumano jugador1) {
-        jugador1.getMano();
-        jugador1.jugarCarta(ES.leerEntero("Elige la carta que deseas jugar: ", 1, jugador1.tamMano()));
+    public static void pedirCarta(JugadorHumano jugadorA) {
+        jugadorA.getMano();
+        jugadorA.jugarCarta(ES.leerEntero("Elige la carta que deseas jugar: ", 1, jugadorA.tamMano()));
     }
 
-    public void resultadoRonda(String nombreGanador) {
+    public static void resultadoRonda(String nombreGanador) {
          System.out.println(nombreGanador);
     }
 
-    public void resultadoPartida(String nombreGanadorPartida, int puntos) {
+    public static void resultadoPartida(String nombreGanadorPartida, int puntos) {
         System.out.println("Ha ganado: "+nombreGanadorPartida + " \nCon: "+puntos);
     }
 
-    public void estadoPartida(Naipe naipe, int cartasRestantes) {
+    public static void estadoPartida(Naipe naipe, int cartasRestantes) {
         System.out.println("Carta de muestra:"+naipe.toString());
         System.out.println("Cartas restantes:"+cartasRestantes);
     }
 
-    public void cartaJugadaOponente(String nombreJugadorB, Naipe naipe) {
+    public static void cartaJugadaOponente(String nombreJugadorB, Naipe naipe) {
         System.out.println("Nombre: "+nombreJugadorB);
         System.out.println("La carta jugada es: "+ naipe.toString());
     }
